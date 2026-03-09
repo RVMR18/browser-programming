1fb0e72bf5a4b505af46df5832c24e0152f90008
RVMR18 <madushanedu93@gmail.com>
2026-03-10 00:21:41 +0530

    updated async

--- lecture-6 exercsie/exercises/exercise.js
+++ lecture-6 exercsie/exercises/exercise.js
@@ -106,7 +106,7 @@ async function runFetchExample() {
 
         // Send request to server
         const response = await fetch(
-            "https://jsonplaceholder.typicode.com/todos/999999"
+            "https://jsonplaceholder.typicode.com/todos/1"
         );
 
         // Check if HTTP status is OK (200–299)
